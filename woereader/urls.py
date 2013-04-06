0d7e9d95942fd6032413fb34f5d0bd6dd8add3c6
woegjiub <woegjiub@gmail.com>
2013-04-06 15:27:03 +1100

    preliminary viewport-sizing of all content

--- woereader/urls.py
+++ woereader/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from woereader.forms import FormWithCaptcha
 from registration.views import register
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,5 +14,7 @@ urlpatterns = patterns(
         name='registration_register'),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^accounts/profile/$', 'ownreader.views.index', name='index'),
+    url(r'^accounts/register/$', TemplateView.as_view(
+        template_name='registration/activate.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
