871af7c999a3dba53853b143abc55893d835baa1
woegjiub <woegjiub@gmail.com>
2013-05-05 21:01:50 +1000

    fixed menus and middle click, added captions

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,60 @@
+//Startup
 var canTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
 $(document).ready(prepare);
 $(window).resize(resizeContent);
-
 function prepare() {
+	addCaptions();
 	setupTouch();
 	setupMenus();
 	resizeContent();
 }
 
+//UI  setup functions
+function setupTouch() {
+	if(canTouch) {
+		$("html").addClass("touch");
+	}
+}
+function menuShow($this){
+	$this.find('ul').css('display', 'block');
+	menuHide($('.menu').not($this));
+}
+function menuHide($this){
+	$this.find('ul').css('display', 'none');
+}
+function menuToggle($this){
+	if($this.find('ul').css('display')=='none')
+		menuShow($this);
+	else
+		menuHide($this);
+}
+function setupMenus() {
+	//Menus should have null locations; don't propagate links otherwise
+	$('a').on('click touchstart', (function(e){
+		if($(this).attr('href')!='#')
+			e.stopPropagation();
+	}));
+	//Bind js alternatives to the CSS :hover, hides all but selected
+	$('.menu:has(ul)').on('click touchstart', (function(e){
+		e.stopPropagation(); //stop it from triggering the document event
+		e.preventDefault();  //touchscreen devices do both otherwise
+		menuToggle($(this));
+	}));
+	$('.menu').on('mouseenter', (function(){ menuShow($(this)) }));
+	$('.menu').on('mouseleave', (function(){ menuHide($(this)) }));
+	//Close menus on outside clicks
+	$(document).on('click touchstart', (function (e) {
+		if(e.which != (2 || 3)){
+			menuHide($('.menu'));
+		}
+	}));
+	//Scroll to top on Navbar click
+	$('#navbar').click(function (e) {
+		window.scrollTo(0,0);
+	});
+}
+
+//Appearance fixer functions
 function resizeContent(){
 	//Workaround for browsers who don't support CSS calc()
 	var properHeight = $(window).height() - $('#navbar').height();
@@ -15,38 +62,13 @@ function resizeContent(){
 		$('#main').css('height', properHeight);
 	}
 }
-
-function setupMenus() {
-	//Binds click alternatives to the CSS :hover, hides all but selected
-	$('nav li').click(function (e) {
-		e.stopPropagation();
-		$(this).find('ul').toggle();
-		$('nav li').not(this).find('ul').hide();
-		//Don't follow links to '#'
-		if($(this).find('a').attr('href')=="#"){
-			return false;
-		}
-	});
-	//Close menus on outside clicks
-	$(document).click(function (e) {
-		e.stopPropagation();
-		$('nav li ul').hide();
-	});
-	//Scroll to top on Navbar click
-	$('#navbar').click(function (e) {
-		e.stopPropagation;
-		window.scrollTo(0,0);
-	});
-	//Because somehow, mobile browsers will otherwise require 2 clicks on menus
-	$('#navbar').click();
+function addCaptions(){
+	var str = $('body').html();
+	var re = /(<img[^>]*title=")([^"]*)("[^>]*>)/gi;
+	$('body').html(str.replace(re, "$1$2$3<br />$2<br />"));
 }
 
-function setupTouch() {
-	if(canTouch) {
-		$("html").addClass("touch");
-	}
-}
-
+//Functions for django+ajax
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie != '') {
@@ -62,24 +84,28 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
-
-function toggleRead(itemId){
+function djajax($url, $data){
 	var csrftoken = getCookie('csrftoken');
 	$.ajax({
-		url: "toggleRead",
+		url: $url,
 		crossDomain: false,
 		type: "POST",
 		beforeSend: function(xhr, settings) {
-	    	if (!csrfSafeMethod(settings.type)) {
-	        	xhr.setRequestHeader("X-CSRFToken", csrftoken);
-	        }
-	    },
-		data: { id: itemId }
+			if (!csrfSafeMethod(settings.type)) {
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		},
+		data: $data
 	});
 	return false;
 }
+
+//UI element response functions
+function toggleRead(itemId){
+	djajax("toggleRead", { id: itemId });
+}
+
